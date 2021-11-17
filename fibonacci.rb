15abbe6856c99f318de34099fa378e09f6d1c5cf
root <root@LAPTOP-F86AFOMK.localdomain>
2021-11-17 23:25:36 +0800

    遞迴

---
+++ fibonacci.rb
@@ -0,0 +1,16 @@
+fib = Enumerator.new do |y|
+  a = b = 1
+  loop do
+    y << a #做完放到y陣列
+    a, b = b, a + b
+  end
+end
+
+fib.take(5)
+
+#遞迴解法，需要有停止鍵
+def fib(n)
+  return n if n <= 2
+  return fib(n-1) + fib(n-2)
+  
+  fib(4)
